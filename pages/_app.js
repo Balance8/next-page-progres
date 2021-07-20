b916a405027b624d20f7790b4d0b1dd03bfeafc7
Balance8 <mslocum819@gmail.com>
2021-07-20 11:23:17 -0700

    useState loading bar tutorial

---
+++ pages/_app.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import 'tailwindcss/tailwind.css';
+import { useRouter } from 'next/router';
+
+import { Progress } from '../components';
+import { useProgressStore } from '../store';
+
+function MyApp({ Component, pageProps }) {
+  const [isAnimating, setIsAnimating] = useState(false);
+  // const setIsAnimating = useProgressStore((state) => state.setIsAnimating);
+  // const isAnimating = useProgressStore((state) => state.isAnimating);
+  const router = useRouter();
+  useEffect(() => {
+    const handleStart = () => {
+      setIsAnimating(true);
+    };
+    const handleStop = () => {
+      setIsAnimating(false);
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+  return (
+    <>
+      <Progress isAnimating={isAnimating} />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
